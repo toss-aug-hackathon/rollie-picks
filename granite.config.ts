eeb61f213e45f8cda1ce5249e412d916a1911ba6
LeeTheY <peter20011148@gmail.com>
2026-08-14 22:30:11 +0900

    fix: restore non-game navigation

--- granite.config.ts
+++ granite.config.ts
@@ -36,7 +36,7 @@ export default defineConfig({
     withHomeButton: false,
   },
   webViewProps: {
-    type: 'game',
+    type: 'partner',
     bounces: false,
     pullToRefreshEnabled: false,
     overScrollMode: 'never',
